cec8d6f743260c84eef03319e03290af8b4022bf
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-05-29 10:52:54 -0400

    header displays number of unread notifications

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 import getFirstChar from "../util/getFirstChar";
+import getNotifications from "../api/getNotifications";
+import { useQuery } from "@tanstack/react-query";
 
-export default function Header({user}) {
+export default function Header({ user }) {
+  const { data, isLoading } = useQuery(["notifications", user?.id], () =>
+    getNotifications(user?.id)
+  );
 
-
-
-    return (
-        <header className="fixed top-0 left-0 w-full bg-gray-900 text-white p-4 flex justify-between items-center">
-            <div></div>
-            <h1>Chesstopia</h1>
-            <div>{user ? getFirstChar(user.email) : ""}</div>
-        </header>
-    );
+  return (
+    <header className="fixed top-0 left-0 w-full bg-gray-900 text-white p-4 flex justify-between items-center">
+      <div></div>
+      <h1>Chesstopia</h1>
+      <div className="flex gap-2">
+          <div>{user ? getFirstChar(user.email) : ""}</div>
+          <div>{data?.filter(n => !n.read).length}</div>
+      </div>
+    </header>
+  );
 }
