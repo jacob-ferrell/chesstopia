870f03baea3a13673fc350436edb403c20e3e32b
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-05-29 10:52:25 -0400

    game is refreshed when chosen

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -1,10 +1,13 @@
+import getGame from "../api/getGame";
+
 export default function GameInfo({game, setGame, user}) {
 
     const opponent = game.players.find(p => p.id !== user.id);
 
-    function handleClick(e) {
+    async function handleClick(e) {
         e.preventDefault();
-        setGame(game);
+        const res = await getGame(game?.id);
+        if (res.status === 200) setGame(res.data);
     }
 
     return(
