1d47449401092b951e8313134f59b43e884f09ef
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-05-30 18:16:45 -0400

    prevent api call if there's no token

--- src/api/getNotifications.js
+++ src/api/getNotifications.js
@@ -1,6 +1,7 @@
 import axiosInstance from "../axios";
 
 export default async function getNotifications() {
+    if (!localStorage.getItem("token")) return null;
     const res = await axiosInstance.get(`notifications`);
     if (res.status === 200) return res.data;
 }
