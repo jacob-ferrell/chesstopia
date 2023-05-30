02cb677a11ee528b22c74032941de2cda0d3aceb
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-05-30 18:20:59 -0400

    added refetch

--- src/hooks/useCurrentUser.jsx
+++ src/hooks/useCurrentUser.jsx
@@ -2,10 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import getCurrentUser from "../api/getCurrentUser";
 
 export default function useCurrentUser() {
-    const { data, isLoading, isError } = useQuery(["user"], getCurrentUser);
-    return {
-        user: data,
-        isLoading,
-        isError
-    }
+  const { data, isLoading, isError, refetch } = useQuery(
+    ["user"],
+    getCurrentUser
+  );
+  return {
+    user: data,
+    isLoading,
+    isError,
+    refetch,
+  };
 }
