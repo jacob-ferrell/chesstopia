d7d2ca2e1ab78ede6e31895f95f04e4ae5ee57ff
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-05-30 18:16:25 -0400

    prevent api call if there's no token

--- src/api/getCurrentUser.js
+++ src/api/getCurrentUser.js
@@ -2,6 +2,7 @@ import axiosInstance from "../axios";
 import { useNavigate } from "react-router-dom";
 
 export default async function getCurrentUser() {
+    if (!localStorage.getItem("token")) return null;
     const res = await axiosInstance.get('current-user');
     if (res.status === 200) return res.data;
     return null;
