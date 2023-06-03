1b484e44fc5148b82b9ce2805454cd4537c8a594
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-03 09:54:08 -0400

    defined fetch function within hook so it can navigate to "/login" on 403 error

--- src/hooks/useCurrentUser.jsx
+++ src/hooks/useCurrentUser.jsx
@@ -1,11 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
-import getCurrentUser from "../api/getCurrentUser";
-
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../axios";
 export default function useCurrentUser() {
   const { data, isLoading, isError, refetch } = useQuery(
     ["user"],
     getCurrentUser
   );
+
+  async function getCurrentUser() {
+    if (!localStorage.getItem("token")) return null;
+    console.log(axiosInstance);
+    const res = await axiosInstance.get("current-user");
+    if (res.status === 403) return useNavigate()("/login");
+    if (res.status === 200) return res.data;
+    return null;
+  }
+
   return {
     user: data,
     isLoading,
