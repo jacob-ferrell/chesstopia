fb8e89128c183d81566717f032ea21606429fbf2
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-03 09:53:19 -0400

    created AddFriendModal

---
+++ src/components/modals/AddFriendModal.jsx
@@ -0,0 +1,11 @@
+import AddFriendForm from "../AddFriendForm";
+import Modal from "./Modal";
+
+export default function AddFriendModal({isOpen, closeModal}) {
+
+    return (
+        <Modal title={"Add Friend"} isOpen={isOpen} closeModal={closeModal}>
+            <AddFriendForm close={closeModal} />
+        </Modal>
+    );
+}
