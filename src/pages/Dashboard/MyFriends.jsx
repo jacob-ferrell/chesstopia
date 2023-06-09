33902f07b497c332a246d36057167666d321131e
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-09 16:28:11 -0400

    Moved into Tab. Changed styling and added friend stats

--- src/pages/Dashboard/MyFriends.jsx
+++ src/pages/Dashboard/MyFriends.jsx
@@ -1,56 +1,59 @@
-import useCreateGame from "../../hooks/useCreateGame";
 import { useState } from "react";
 import useFriends from "../../hooks/useFriends";
 import AddFriendModal from "../../components/modals/AddFriendModal";
+import getFriendStats from "../../util/getFriendStats";
+import useGames from "../../hooks/useGames";
+import useCurrentUser from "../../hooks/useCurrentUser";
+import FriendModal from "../../components/modals/FriendModal";
 
 export default function MyFriends() {
   const { friends, isLoading } = useFriends();
-
-  const createGame = useCreateGame();
+  const { games, gamesLoading } = useGames();
 
   const [showModal, setShowModal] = useState(false);
 
-  async function handleChallengeClick(e) {
-    e.preventDefault();
-    await createGame(e.target.dataset.id);
-
-  }
-
-  function renderFriends() {
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
-
-    if (!friends || friends.length === 0) {
-      return <p>No friends found.</p>;
-    }
-
-    return friends.map((f) => (
-      <div key={`f-${f.id}`}>
-        {f.email}{" "}
-        <a onClick={handleChallengeClick} href="" data-id={f.id}>
-          Challenge
-        </a>
-      </div>
-    ));
-  }
+  if (isLoading || gamesLoading) return <div>Loading...</div>;
 
   return (
-    <div className="border-solid border-2 border-black rounded-md p-4 bg-gray-500 text-white">
-      <div className="flex gap-3 justify-center items-center">
-        <h1 className="text-4xl">My Friends</h1>
-        <button
-          className="bg-purple-500 w-12 h-12 rounded-full"
-          onClick={() => setShowModal(true)}
-        >
-          <span className="text-white text-2xl font-bold">+</span>
-        </button>
-      </div>
-      <AddFriendModal
-        closeModal={() => setShowModal(false)}
-        isOpen={showModal}
-      />
-      {renderFriends()}
-    </div>
+    <>
+      <FriendModal isOpen={showModal} closeModal={() => setShowModal(false)} />
+      <ul>
+        <li className="flex justify-end mb-2 border-b border-gray-200 pb-3">
+          <button
+            type="button"
+            onClick={() => setShowModal(true)}
+            className="rounded-md bg-lime-700 shadow px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          >
+            Add Friend
+          </button>
+        </li>
+        {!friends.length ? (
+          <li>
+            <div>User has no friends</div>
+          </li>
+        ) : (
+          friends?.map((friend) => {
+            const stats = getFriendStats(games, friend);
+            return (
+              <li
+                key={friend.id}
+                className="relative rounded-md p-3 hover:bg-gray-100"
+              >
+                <h3 className="text-sm font-medium leading-5 text-left">
+                  {friend.email}
+                </h3>
+                <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
+                  <li>{`Open Games: ${stats.open.length}`}</li>
+                  <li>&middot;</li>
+                  <li>{`Won Against: ${stats.won.length}`}</li>
+                  <li>&middot;</li>
+                  <li>{`Lost Against: ${stats.lost.length}`}</li>
+                </ul>
+              </li>
+            );
+          })
+        )}
+      </ul>
+    </>
   );
 }
