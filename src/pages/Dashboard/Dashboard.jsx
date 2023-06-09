da00bdda2a71ac37910e72bb2accce736454ec77
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-09 16:27:39 -0400

    added Tabs

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,17 +2,19 @@ import MyGames from "./MyGames";
 import MyFriends from "./MyFriends";
 import MyNotifications from "./MyNotifications";
 import { useNavigate } from "react-router-dom";
+import Tabs from "./Tabs";
 
 export default function Dashboard({ setGame }) {
   const navigate = useNavigate();
   return (
-    <div className="w-full h-full pt-4">
-      <div className="flex justify-between w-full px-12">
-        <MyGames setGame={setGame} />
-        <MyFriends />
-        <MyNotifications />
-      </div>
-      <button onClick={() => navigate("/lobby")}>Quick Play</button>
+    <div className="w-full h-full pt-4 flex flex-col gap-2 items-center">
+      <button
+        className="w-32 h-8 rounded bg-gray-700 text-gray-100 font-bold hover:bg-opacity-30"
+        onClick={() => navigate("/lobby")}
+      >
+        Quick Play
+      </button>
+      <Tabs setGame={setGame} /> 
     </div>
   );
 }
