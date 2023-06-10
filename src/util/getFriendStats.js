5624940f21ab278dc236705972f828ac46051bfb
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-06-10 10:45:32 -0400

    created helper function for getting stats against a friend

---
+++ src/util/getFriendStats.js
@@ -0,0 +1,24 @@
+export default function getFriendStats(games, friend) {
+    const friendGamesMap = {
+        open: [],
+        won: [],
+        lost: [],
+    }
+    const gamesWithFriend = games.filter(game => {
+        return game?.whitePlayer?.id === friend?.id || game?.blackPlayer?.id === friend?.id;
+    })
+
+    for (let game of gamesWithFriend) {
+        if (game.winner === null) {
+            friendGamesMap.open.push(game);
+            continue;
+        }
+        if (game.winner.id === friend.id) {
+            gamesWithFriend.lost.push(game);
+            continue;
+        }
+        gamesWithFriend.won.push(game);
+
+    }
+    return friendGamesMap;
+}
