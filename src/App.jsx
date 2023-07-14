b9f0f6ceb036761a21056b6fdba62c398390e1f1
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-14 13:45:33 -0400

    routes to login if user is not verified

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Game from "./pages/Game/Game";
 import { Client } from "@stomp/stompjs";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 import LoginPage from "./pages/Login/LoginPage";
-import Dashboard from "./pages/Dashboard/Dashboard"
-import { Routes, Route, useNavigate, useParams } from "react-router";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import { Routes, Route, useNavigate } from "react-router";
 import Header from "./components/Header";
 import useCurrentUser from "./hooks/useCurrentUser";
 import Lobby from "./pages/Lobby/Lobby";
+import SignUpPage from "./pages/SignUp/SignUpPage";
 
 function App() {
   const [selectedGame, setSelectedGame] = useState(null);
@@ -17,19 +18,18 @@ function App() {
 
   const queryClient = useQueryClient();
   const navigate = useNavigate();
-  const { gameId, userId } = useParams();
   const { user, isLoading } = useCurrentUser();
 
   useEffect(() => {
-    if (!isLoading && user === null) {
-      return navigate("/login");
-    }
-  }, [isLoading, user, user?.id]);
+    if (isLoading || user !== null) return;
+    if (location.pathname.includes("signup")) return;
+    navigate("/login");
+  }, [isLoading, user, location.pathname]);
 
   useEffect(() => {
     if (!user) return;
     const client = new Client({
-      brokerURL: `ws://localhost:8080/websocket`,
+      brokerURL: `ws://45.4.172.7:8081/chess-0.0.1-SNAPSHOT/websocket`,
       onConnect: () => {
         console.log("Connected");
         const subscription = client.subscribe(
@@ -47,20 +47,34 @@ function App() {
     };
   }, [user?.id]);
 
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/dashboard");
+    }
+  }, [location.pathname]);
+
   return (
-    <div className="App w-full pt-[80px] h-full bg-gradient-to-t from-gray-700 to-gray-900">
+    <div className="App w-full pt-[80px] h-full bg-gradient-to-t from-gray-700 to-gray-900 flex justify-center">
       <Header />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/lobby" element={<Lobby stompClient={stompClient}/>} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/lobby" element={<Lobby stompClient={stompClient} />} />
         <Route
           path={"/"}
           element={<Dashboard setGame={setSelectedGame} game={selectedGame} />}
         />
         <Route
-          path={"/dashboard"}
-          element={<Dashboard setGame={setSelectedGame} game={selectedGame} />}
-        />
+          path={"/dashboard/*"}
+          element={
+            <Dashboard
+              setGame={setSelectedGame}
+              game={selectedGame}
+            />
+          }
+        >
+          
+        </Route>
         <Route
           path="/game/:gameId"
           element={
