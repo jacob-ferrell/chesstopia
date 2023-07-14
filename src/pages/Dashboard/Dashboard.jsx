f3f187570b1a61749ddf08750d2b36fba9ea0035
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-14 13:47:38 -0400

    moved quickplay button to tabs

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,19 +1,9 @@
-import MyGames from "./MyGames";
-import MyFriends from "./MyFriends";
-import MyNotifications from "./MyNotifications";
-import { useNavigate } from "react-router-dom";
 import Tabs from "./Tabs";
 
 export default function Dashboard({ setGame }) {
-  const navigate = useNavigate();
+
   return (
-    <div className="w-full h-full pt-4 flex flex-col gap-2 items-center">
-      <button
-        className="w-32 h-8 rounded bg-gray-700 text-gray-100 font-bold hover:bg-opacity-30"
-        onClick={() => navigate("/lobby")}
-      >
-        Quick Play
-      </button>
+    <div className="w-full h-full pt-4 flex flex-col gap-2 items-center"> 
       <Tabs setGame={setGame} /> 
     </div>
   );
