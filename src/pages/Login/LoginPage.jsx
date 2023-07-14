1c79ebbac89913ebbab050e4f0321d2466465cbb
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-14 13:51:16 -0400

    added styling and redirect if user is logged in

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,18 +1,26 @@
 import LoginForm from "./LoginForm";
 import { useLoginUser } from "../../hooks/useLoginUser";
 import { useEffect } from "react";
+import useCurrentUser from "../../hooks/useCurrentUser";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
-  const { credentials, setCredentials, loginUser, isLoading } = useLoginUser();
+  const { user, isLoading: userIsLoading } = useCurrentUser();
+
+  const { credentials, setCredentials, loginUser } = useLoginUser();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    localStorage.removeItem("token");
-  }, []);
+    if (userIsLoading || user === null || location.pathname.includes("signup"))
+      return;
+    navigate("/dashboard");
+  }, [userIsLoading, location.pathname]);
 
-  function loginAsJacob() {
+  /*  function loginAsJacob() {
     setCredentials({
       email: "boomkablamo@gmail.com",
-      password: "asdf",
+      password: "",
     });
     loginUser();
   }
@@ -20,21 +28,27 @@ export default function LoginPage() {
   function loginAsCindy() {
     setCredentials({
       email: "cindy@gmail.com",
-      password: "asdf",
+      password: "",
     });
     loginUser();
-  }
+  } */
 
   return (
-    <div>
+    <div className="flex flex-col gap-6 p-4 max-w-xl w-full">
+      <h2 className="text-gray-100 text-3xl font-medium">Login</h2>
       <LoginForm
         credentials={credentials}
         setCredentials={setCredentials}
         login={loginUser}
       />
-      <button>Register</button>
-      <button onClick={loginAsJacob}>Login As Jacob</button>
-      <button onClick={loginAsCindy}>Login As Cindy</button>
+      <button
+        onClick={() => navigate("/signup")}
+        className="text-gray-100 bg-gradient-to-b from-sky-900 to-sky-600 hover:brightness-125 shadow font-medium rounded py-2"
+      >
+        Register
+      </button>
+      {/* <button onClick={loginAsJacob}>Login As Jacob</button>
+      <button onClick={loginAsCindy}>Login As Cindy</button> */}
     </div>
   );
 }
