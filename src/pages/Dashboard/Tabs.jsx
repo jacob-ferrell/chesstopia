e8355e99482ffa3127b4460aa71b7d4d0a51f9ee
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-14 13:48:05 -0400

    tabs route to corresponding pages using refs

--- src/pages/Dashboard/Tabs.jsx
+++ src/pages/Dashboard/Tabs.jsx
@@ -1,8 +1,16 @@
 import { Tab } from "@headlessui/react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import MyFriends from "./MyFriends";
 import MyGames from "./MyGames";
 import MyNotifications from "./MyNotifications";
+import { useNavigate } from "react-router-dom";
+import useNotifications from "../../hooks/useNotifications";
+
+const pageMap = {
+  games: "My Games",
+  friends: "My Friends",
+  notifications: "Notifications",
+};
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -12,13 +20,45 @@ export default function Tabs({ setGame }) {
   let [pages] = useState({
     "My Games": <MyGames setGame={setGame} classNames={classNames} />,
     "My Friends": <MyFriends />,
-    "Notifications": <MyNotifications />,
+    Notifications: <MyNotifications />,
   });
 
+  const tabRefs = {
+    "My Games": useRef(null),
+    "My Friends": useRef(null),
+    Notifications: useRef(null),
+  };
+
+  const navigate = useNavigate();
+  const { notifications, isLoading } = useNotifications();
+
+  function getUnreadNotifications() {
+    if (isLoading || !notifications) return;
+    return notifications.filter((n) => !n.read).length;
+  }
+
+  function handleTabClick(page) {
+    navigate(
+      `/dashboard/${Object.keys(pageMap).find((k) => pageMap[k] === page)}`
+    );
+  }
+
+  useEffect(() => {
+    const path = location.pathname.split("/").at(-1);
+    if (!Object.keys(pageMap).includes(path)) return tabRefs["My Games"].current.click();
+    tabRefs[pageMap[path]].current.click();
+  }, [location.pathname]);
+
   return (
-    <div className="w-full max-w-md px-2 py-16 sm:px-0">
+    <div className="w-full max-w-xl px-2 py-6 sm:px-0">
+      <button
+        className="w-full p-3 mb-2 rounded-xl bg-gradient-to-b from-gray-500 to-gray-700 font-medium"
+        onClick={() => navigate("/lobby")}
+      >
+        Quick Play
+      </button>
       <Tab.Group>
-        <Tab.List className="flex space-x-1 rounded-xl bg-gray-700 p-1">
+        <Tab.List className="flex space-x-1 rounded-xl bg-gradient-to-b from-gray-500 to-gray-700 p-1">
           {Object.keys(pages).map((page) => (
             <Tab
               key={page}
@@ -27,12 +67,18 @@ export default function Tabs({ setGame }) {
                   "w-full rounded-lg py-2.5 text-sm font-medium leading-5",
                   "ring-white ring-opacity-60 ring-offset-2 ring-offset-gray-400 focus:outline-none focus:ring-2",
                   selected
-                    ? "bg-white shadow text-gray-900"
+                    ? "bg-gray-50 shadow text-gray-900"
                     : "text-gray-100 hover:bg-white/[0.12]"
                 )
               }
+              ref={tabRefs[page]}
+              onClick={() => handleTabClick(page)}
             >
-              {page}
+              {page === "Notifications" &&
+              !isLoading &&
+              getUnreadNotifications()
+                ? page + `(${getUnreadNotifications()})`
+                : page}
             </Tab>
           ))}
         </Tab.List>
