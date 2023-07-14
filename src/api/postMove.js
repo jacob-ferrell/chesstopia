ad094f28338ad1a9b32706dae77ed9d4734c33ad
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-14 13:38:41 -0400

    added optional param promotiontype

--- src/api/postMove.js
+++ src/api/postMove.js
@@ -1,8 +1,8 @@
 import axiosInstance from "../axios";
 
-export default async function postMove(gameId, x, y, y1, x1) {
+export default async function postMove(gameId, x, y, y1, x1, upgradeType = "") {
   const res = await axiosInstance.post(
-    `game/${gameId}/move?x0=${x}&y0=${y}&x1=${x1}&y1=${y1}`
+    `game/${gameId}/move?x0=${x}&y0=${y}&x1=${x1}&y1=${y1}&promotion=${upgradeType}`
   );
   return res;
 }
