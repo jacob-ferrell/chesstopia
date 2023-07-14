9cfdb3578e22a62ee1f9774027f8d56045bfecfc
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-14 13:51:02 -0400

    added styling

--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -1,23 +1,35 @@
-export default function LoginForm({
-  credentials,
-  setCredentials,
-  login,
-}) {
+export default function LoginForm({ credentials, setCredentials, login }) {
   return (
-    <form onSubmit={login} className="flex flex-col">
-      <input
-        type="email"
-        value={credentials.email}
-        onChange={(e) => setCredentials(prev => ({...prev, email: e.target.value}))}
-        placeholder="Enter Email"
-      ></input>
-      <input
-        type="password"
-        value={credentials.password}
-        onChange={(e) => setCredentials(prev => ({...prev, password: e.target.value}))}
-        placeholder="Enter Password"
-      ></input>
-      <button type="submit">Login</button>
+    <form onSubmit={login} className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email" className="text-gray-100">
+          Email
+        </label>
+        <input
+          type="email"
+          value={credentials.email}
+          onChange={(e) =>
+            setCredentials((prev) => ({ ...prev, email: e.target.value }))
+          }
+          placeholder="Enter Email"
+          className="rounded bg-gray-100 text-gray-900 px-2 py-1"
+          id="email"
+        ></input>
+        <label htmlFor="password" className="text-gray-100">
+          Password
+        </label>
+        <input
+          type="password"
+          value={credentials.password}
+          onChange={(e) =>
+            setCredentials((prev) => ({ ...prev, password: e.target.value }))
+          }
+          placeholder="Enter Password"
+          className="rounded bg-gray-100 text-gray-900 px-2 py-1"
+          id="password"
+        ></input>
+      </div>
+      <button type="submit" className="bg-gradient-to-b from-lime-900 to-lime-700 hover:brightness-125 font-medium text-gray-100 shadow rounded py-2">Login</button>
     </form>
   );
 }
