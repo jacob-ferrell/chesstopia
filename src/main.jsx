1b88a3e4d7f044678d5a7dbb1e4136f32d675473
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-20 11:21:44 -0400

    changed to hashrouter due to lack of server-side routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 const domain = "dev-34s13r3ilknohwmq.us.auth0.com";
 const clientId = "27F4sjcVpWzVsviGDpdjk13Z5rmdywV7";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter baseline="/">
       <QueryClientProvider client={queryClient}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
