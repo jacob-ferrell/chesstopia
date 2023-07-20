2b60aa976124d8cb76d92418ac47a9c9c50129bd
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-20 14:16:30 -0400

    changed signup to handle creating guest user

--- src/hooks/useSignUp.jsx
+++ src/hooks/useSignUp.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axiosInstance from "../axios";
 import { useNavigate } from "react-router-dom";
 import useCurrentUser from "./useCurrentUser";
+import generateRandomString from "../util/generateRandomString";
 
 export default function useSignUp() {
   const [credentials, setCredentials] = useState({
@@ -14,13 +15,24 @@ export default function useSignUp() {
   const { refetch } = useCurrentUser();
   const navigate = useNavigate();
 
-  async function signUp(e) {
-    e.preventDefault();
+  async function signUp(e = null) {
+    e?.preventDefault();
+    if (!e) {
+      const password = generateRandomString(15);
+      setCredentials({
+        email: `Guest${generateRandomString(5)}@${generateRandomString(6)}.com`,
+        password,
+        confirmPassword: password,
+        firstName: "Guest",
+        lastName: "User",
+      });
+    }
     try {
       if (credentials.password !== credentials.confirmPassword) {
         throw new Error("Passwords must match");
       }
       const { email, password, firstName, lastName } = credentials;
+      console.log(email, password, firstName, lastName);
       await axiosInstance.post("/auth/register", {
         email,
         password,
@@ -29,7 +41,6 @@ export default function useSignUp() {
       });
       await refetch();
       navigate("/dashboard");
-      
     } catch (error) {
       console.log(error);
       alert(error);
