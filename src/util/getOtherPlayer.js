abddbcda993d5fa2c5919b59ea978779e7cc2d3d
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-22 07:55:57 -0400

    added handling for other player being null

--- src/util/getOtherPlayer.js
+++ src/util/getOtherPlayer.js
@@ -1,6 +1,6 @@
 export default function getOtherPlayer(game, currentUser) {
   const { whitePlayer, blackPlayer } = game;
   return [whitePlayer, blackPlayer].find(
-    (user) => user.email !== currentUser.email
+    (user) => user?.email !== currentUser?.email
   );
 }
